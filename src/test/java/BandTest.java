bd4322386e14680c1d4452761f341a53c7f1688a
austinminnon <whi7ee@gmail.com>
2016-03-04 11:17:22 -0800

    add passing test for empty Band list

---
+++ src/test/java/BandTest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.lang.WordUtils;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BandTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Band.all().size(), 0);
+  }
+}
